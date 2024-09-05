76e8a44f6d13dfd5b67755a3048a35c70d4ca27a
ShinJaehun <gunsmoke.shin@gmail.com>
2024-09-05 15:47:19 +0900

    fiish user roles

---
+++ app/models/post.rb
@@ -0,0 +1,10 @@
+class Post < ApplicationRecord
+    validates :title, :user, presence: true
+    belongs_to :user
+
+    resourcify
+
+    def to_s 
+        title
+    end
+end
---
+++ app/models/user.rb
@@ -0,0 +1,16 @@
+class User < ApplicationRecord
+  rolify
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  has_many :posts
+
+  # include Roleable
+
+  has_many :posts, through: :roles, source: :resource, source_type: :Post
+  has_many :created_posts, -> { where(roles: {name: :creator}) }, through: :roles, source: :resource, source_type: :Post
+  has_many :edited_posts, -> { where(roles: {name: :editor}) }, through: :roles, source: :resource, source_type: :Post
+
+end
